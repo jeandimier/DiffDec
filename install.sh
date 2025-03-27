943515780e862a5f824fa99c018039cfc4bc5360
jeandimier <jean.dimier-de-la-brunetiere@aqemia.com>
2025-03-27 14:54:34 +0000

    env changes

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 conda deactivate
 conda env create -f env.yml
 conda activate diffdec
-pip install awswrangler requests-auth-aws-sigv4 mlflow
+pip install awswrangler requests-auth-aws-sigv4 torch==2.4.0 numpy imageio
